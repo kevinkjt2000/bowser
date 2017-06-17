6c6e2f8e041e1eb6ebb2b6cf473942cb7f8600ef
Kevin Tindall <kevinkjt2000@gmail.com>
2017-06-17 09:42:07 -0500

    convert to bot syntax

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-import discord
+from discord.ext import commands
 from mcstatus import MinecraftServer
 
-client = discord.Client()
+description = '''A bot for querying the status of a minecraft server.'''
+bot = commands.Bot(command_prefix='!', description=description)
 servers = [
     MinecraftServer(host='KnightOfficial.Playat.CH', port=25565)
 ]
 
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!'):
-        status = servers[0].status()
-        online_players = [p.name for p in status.players.sample]
-        online_players = str(online_players).replace("'", '')
-        online_count = status.players.online
-        max_count = status.players.max
-        mods_count = len(status.raw['modinfo']['modList'])
-        print(message.content)
-        status_message = '{} mods loaded, players {}/{}: {}'.format(
-            mods_count, online_count, max_count, online_players)
-        await client.send_message(message.channel, status_message)
+@bot.command(description='For getting the status')
+async def status():
+    status = servers[0].status()
+    online_players = [p.name for p in status.players.sample]
+    online_players = str(online_players).replace("'", '')
+    online_count = status.players.online
+    max_count = status.players.max
+    mods_count = len(status.raw['modinfo']['modList'])
+    status_message = '{} mods loaded, players {}/{}: {}'.format(
+        mods_count, online_count, max_count, online_players)
+    await bot.say(status_message)
 
 token = open('token.txt').read().replace('\n', '')
-client.run(token)
+bot.run(token)
