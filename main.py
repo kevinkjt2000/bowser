151a0fe75783790530a9a6d26667010ee10c2a2d
Kevin Tindall <kevinkjt2000@gmail.com>
2017-06-26 06:38:13 -0500

    differentiate between errors

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ servers = [
 
 
 async def on_command_error(exception, context):
-    await bot.send_message(
-        context.message.channel,
-        'The server is not accepting connections at this time',
-    )
+    if str(context.command) == 'status':
+        await bot.send_message(
+            context.message.channel,
+            'The server is not accepting connections at this time.',
+        )
+    else:
+        await bot.send_message(
+            context.message.channel,
+            'I do not recognize that command.',
+        )
 
 bot.add_listener(on_command_error)
 
@@ -28,6 +34,7 @@ async def status():
     online_count = status.players.online
     max_count = status.players.max
     mods_count = len(status.raw['modinfo']['modList'])
+    del status
     status_message = '{} mods loaded, players {}/{}: {}'.format(
         mods_count, online_count, max_count, online_players)
     await bot.say(status_message)
