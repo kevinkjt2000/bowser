8a6785d0676528968c755eaf223e482ff6c2d1f3
Kevin Tindall <kevinkjt2000@gmail.com>
2017-07-07 23:26:58 -0500

    add initial test; add main function to bot script

--- src/main.py
+++ src/main.py
@@ -8,15 +8,20 @@ bot = commands.Bot(
 )
 
 
-def get_formatted_status_message():
-    ms = MinecraftServer(host='KnightOfficial.Playat.CH', port=25565)
-    status = ms.status()
+def main():
+    bot.add_listener(on_command_error)
+    token = open('token.txt').read().replace('\n', '')
+    bot.run(token)
+
+
+def get_formatted_status_message(minecraft_server=MinecraftServer(
+        host='KnightOfficial.Playat.CH', port=25565)):
+    status = minecraft_server.status()
     online_players = [p.name for p in status.players.sample]
     online_players = str(online_players).replace("'", '')
     online_count = status.players.online
     max_count = status.players.max
     mods_count = len(status.raw['modinfo']['modList'])
-    del ms
     status_message = '{} mods loaded, players {}/{}: {}'.format(
         mods_count, online_count, max_count, online_players)
     return status_message
@@ -34,12 +39,10 @@ async def on_command_error(exception, context):
             'I do not recognize that command.',
         )
 
-bot.add_listener(on_command_error)
-
 
 @bot.command(description='For getting the status')
 async def status():
     await bot.say(get_formatted_status_message())
 
-token = open('token.txt').read().replace('\n', '')
-bot.run(token)
+if __name__ == '__main__':
+    main()
---
+++ test/test_main.py
@@ -0,0 +1,27 @@
+from mcstatus.pinger import PingResponse
+from unittest.mock import patch
+from src.main import get_formatted_status_message
+import re
+
+
+@patch('src.main.MinecraftServer')
+def test__can_fetch_good_server_status(fake_minecraft_server):
+    good_status = PingResponse({
+        'description': 'fake description',
+        'modinfo': {
+            'modList': [],
+        },
+        'players': {
+            'online': 0,
+            'max': 0,
+            'sample': [],
+        },
+        'version': {
+            'name': 'fake name',
+            'protocol': 316,
+        },
+    })
+    fake_minecraft_server.status.return_value = good_status
+    status_regex = re.compile('\d+ mods loaded, players \d+/\d+: \[.*\]')
+    status_message = get_formatted_status_message(fake_minecraft_server)
+    assert status_regex.match(status_message) is not None
