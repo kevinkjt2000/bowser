05b0335ebc5d34ecc4606828bd032ae81357819f
Kevin Tindall <kevinkjt2000@gmail.com>
2017-08-26 07:36:27 -0500

    empty player list does not add the colon at the end

--- src/Minecraft.py
+++ src/Minecraft.py
@@ -12,7 +12,8 @@ class Minecraft:
     def get_formatted_status_message(self):
         status = self.mc_server.status()
         if status.players.online > 0:
-            online_players = ', '.join([p.name for p in status.players.sample])
+            online_players = ': ' + ', '.join(
+                [p.name for p in status.players.sample])
         else:
             online_players = ''
         online_count = status.players.online
@@ -21,6 +22,6 @@ class Minecraft:
         if 'modinfo' in status.raw:
             mods_count = len(status.raw['modinfo']['modList'])
             status_message += '{} mods loaded, '.format(mods_count)
-        status_message += 'players {}/{}: {}'.format(
+        status_message += 'players {}/{}{}'.format(
             online_count, max_count, online_players)
         return status_message
--- test/test_main.py
+++ test/test_main.py
@@ -12,7 +12,7 @@ def test__can_display_server_status_from_vanilla_server(fake_minecraft_server):
         'version': {'name': '1.12.1', 'protocol': 338, },
     })
     fake_minecraft_server.status.return_value = status_vanilla_empty
-    status_re = re.compile('players \d+/\d+: (.*(, )?)*')
+    status_re = re.compile('^players \d+/\d+$')
     mc = Minecraft(test_server=fake_minecraft_server)
     status_message = mc.get_formatted_status_message()
     assert status_re.match(status_message) is not None
@@ -20,14 +20,20 @@ def test__can_display_server_status_from_vanilla_server(fake_minecraft_server):
 
 @patch('src.Minecraft.MinecraftServer')
 def test__can_fetch_modded_server_status(fake_minecraft_server):
-    status_modded_empty = PingResponse({
+    status_modded_online = PingResponse({
         'description': {'text': 'fake description', },
-        'modinfo': {'modList': [], },
-        'players': {'online': 0, 'max': 8, },
-        'version': {'name': 'fake name', 'protocol': 316, },
+        'modinfo': {'modList': [
+            {'modid': 'FML', 'version': '8.0.99.99'},
+            {'modid': 'forge', 'version': '14.22.0.2460'},
+        ], },
+        'players': {'online': 1, 'max': 8, 'sample': [{
+            'id': '291b495c-b03b-46bb-b2c8-e31ad4cdef44',
+            'name': 'fake_name',
+        }, ], },
+        'version': {'name': 'fake_name', 'protocol': 316, },
     })
-    fake_minecraft_server.status.return_value = status_modded_empty
-    status_re = re.compile('\d+ mods loaded, players \d+/\d+: (.*(, )?)*')
+    fake_minecraft_server.status.return_value = status_modded_online
+    status_re = re.compile('^\d+ mods loaded, players \d+/\d+: (.*(, )?)*$')
     mc = Minecraft(test_server=fake_minecraft_server)
     status_message = mc.get_formatted_status_message()
     assert status_re.match(status_message) is not None
