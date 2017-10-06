2356964da01ea21caa91ecc05978af58e2c56021
Kevin Tindall <kevinkjt2000@gmail.com>
2017-10-06 07:39:01 -0500

    add a failing test to temp branch

--- test/test_main.py
+++ test/test_main.py
@@ -5,7 +5,7 @@ import unittest
 
 
 class TestMain(unittest.TestCase):
-    @patch('src.main.main', return_value=42)
+    @patch('src.main.main')
     def test__init_calls_main_once(self, mock_main):
         import src.main
         with patch.object(src.main, '__name__', '__main__'):
@@ -13,7 +13,7 @@ class TestMain(unittest.TestCase):
             mock_main.assert_called_once_with()
 
 
-class TestBotMain(asynctest.TestCase):
+class TestBot(asynctest.TestCase):
     def setUp(self):
         from src.main import Bot
         self.bot = Bot()
@@ -28,3 +28,25 @@ class TestBotMain(asynctest.TestCase):
 
     def tearDown(self):
         self.mock_run.stop()
+
+    @patch('src.main.Minecraft')
+    async def test__sends_error_message_when_connection_refused(
+            self, mock_mc):
+        mock_mc.get_formatted_status_message.side_effect = \
+            ConnectionRefusedError
+        mock_message = MagicMock(
+            spec=discord.Message,
+            author=MagicMock(
+                spec=discord.User,
+                id=800,
+                name='mock_user',
+            ),
+            channel=MagicMock(
+                spec=discord.Channel,
+            ),
+            content='!help',
+            mentions=[],
+        )
+        with asynctest.patch.object(self.bot, 'say') as mock_say:
+            await self.bot.on_message(mock_message)
+            mock_say.assert_called_once_with(mock_message.content)
