aeb33071a823803c790726a162eb1201daaa0265
Kevin Tindall <kevinkjt2000@users.noreply.github.com>
2018-01-08 18:32:04 -0600

    get rid of ClientSession unclosed warning (#25)

--- bowser/main.py
+++ bowser/main.py
@@ -1,10 +1,13 @@
 from bowser.Bot import Bot
+import asyncio
+loop = asyncio.get_event_loop()
 
 
 def main():
     bot = Bot()
     token = open('token.txt').read().replace('\n', '')
     bot.run(token)
+    loop.run_until_complete(bot.close())
 
 
 def init():
