cf9d156bf5611f6b71e962bb2177c38729e5aa86
Kevin Tindall <kevinkjt2000@gmail.com>
2018-03-14 21:52:12 -0500

    refactor(main.py): Use the async loop from bot's client inheritance

--- bowser/main.py
+++ bowser/main.py
@@ -1,15 +1,13 @@
-import asyncio
 from bowser.Bot import Bot
 
 
 def main():
-    loop = asyncio.get_event_loop()
     bot = Bot()
     try:
         token = open('token.txt').read().replace('\n', '')
         bot.run(token)
     except Exception as ex:
-        loop.run_until_complete(bot.close())
+        bot.loop.run_until_complete(bot.close())
         raise ex
 
 
