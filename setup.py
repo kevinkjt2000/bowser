f72406c8eec8b375b0582e9dda66d822997fb072
Kevin Tindall <kevinkjt2000@gmail.com>
2018-03-18 12:47:51 -0500

    build(setup.py): Move call to setup into __main__

--- setup.py
+++ setup.py
@@ -21,16 +21,17 @@ REQUIREMENTS = {
     ],
 }
 
-setup(
-    name='bowser',
-    install_requires=REQUIREMENTS['install'],
-    extras_require=REQUIREMENTS['extras'],
-    setup_requires=REQUIREMENTS['setup'],
-    packages=['bowser'],
-    entry_points={
-        'console_scripts': [
-            'bowser = bowser.main:main',
-        ]
-    },
-    use_scm_version=True,
-)
+if __name__ == '__main__':
+    setup(
+        name='bowser',
+        install_requires=REQUIREMENTS['install'],
+        extras_require=REQUIREMENTS['extras'],
+        setup_requires=REQUIREMENTS['setup'],
+        packages=['bowser'],
+        entry_points={
+            'console_scripts': [
+                'bowser = bowser.main:main',
+            ]
+        },
+        use_scm_version=True,
+    )
