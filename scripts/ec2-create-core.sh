ba4ddfb1c68c24829dbfa16498620798a0538ffe
Kevin Tindall <kevinkjt2000@gmail.com>
2018-03-25 18:31:07 -0500

    ci: Deploy bowser.pex to ec2 and run with docker-compose

--- scripts/ec2-create-core.sh
+++ scripts/ec2-create-core.sh
@@ -1,1 +1,14 @@
-docker-machine create --driver=amazonec2 --amazonec2-ami=ami-3f061b45 --amazonec2-device-name=/dev/xvda --amazonec2-ssh-user=core bowser
+#!/bin/bash
+docker-machine create \
+  --driver=amazonec2 \
+  --amazonec2-ami=ami-3f061b45 \
+  --amazonec2-device-name=/dev/xvda \
+  --amazonec2-ssh-user=core \
+  bowser
+
+docker-machine scp dist/bowser.pex bowser:.
+docker-machine scp servers.json bowser:.
+docker-machine scp token.txt bowser:.
+
+eval $(docker-machine env bowser)
+docker-compose -f docker-compose.yml -f docker-compose.ec2.yml up -d
