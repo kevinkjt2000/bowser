401ce986326d0eee2bd71fbc676b496553ecda15
Kevin Tindall <kevinkjt2000@gmail.com>
2018-04-07 10:33:53 -0500

    feat: Add database class for interfacing with redis

---
+++ bowser/Database.py
@@ -0,0 +1,12 @@
+import redis
+
+
+class Database(object):
+    def __init__(self):
+        self.redis = redis.StrictRedis(host='redis', port=6739, db=0)
+
+    def set_data_of_server(self, server_id, data):
+        self.redis.set(server_id, data)
+
+    def fetch_data_of_server(self, server_id):
+        return self.redis.get(server_id)
