b2550e92647a0d23c53ed95ed8a6cfd679dd77bf
Kevin Tindall <kevinkjt2000@gmail.com>
2018-04-07 21:15:39 -0500

    fix(redis): Fix port number

--- bowser/Database.py
+++ bowser/Database.py
@@ -3,7 +3,7 @@ import redis
 
 class Database(object):
     def __init__(self):
-        self.redis = redis.StrictRedis(host='redis', port=6739, db=0)
+        self.redis = redis.StrictRedis(host='redis', port=6379, db=0)
 
     def set_data_of_server(self, server_id, data):
         self.redis.set(server_id, data)
