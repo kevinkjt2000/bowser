35552aab57e340a8be6db76de68ee10bf7e6d97f
Kevin Tindall <kevinkjt2000@gmail.com>
2018-05-06 10:30:24 -0500

    build: Change to pipenv for python requirements management

--- setup.py
+++ setup.py
@@ -1,34 +1,8 @@
 from setuptools import setup
 
-REQUIREMENTS = {
-    'extras': {
-        'ci': [
-            'codecov',
-        ],
-        'test': [
-            'asynctest',
-            'pytest',
-            'pytest-cov',
-            'pytest-socket',
-        ],
-    },
-    'install': [
-        'discord.py',
-        'mcstatus',
-        'redis',
-        'retrying',
-    ],
-    'setup': [
-        'setuptools_scm',
-    ],
-}
-
 if __name__ == '__main__':
     setup(
         name='bowser',
-        install_requires=REQUIREMENTS['install'],
-        extras_require=REQUIREMENTS['extras'],
-        setup_requires=REQUIREMENTS['setup'],
         packages=['bowser'],
         entry_points={
             'console_scripts': [
