b8442f6503659fbf4787427f3f1a6c3f47f0bf8d
Kevin Tindall <kevinkjt2000@gmail.com>
2018-07-15 09:09:51 -0500

    refactor: Add capability of multiple game channels in tests

--- test/test_bowser.py
+++ test/test_bowser.py
@@ -22,34 +22,37 @@ class HelperFunctions(asynctest.TestCase):
         self.patch_run.start()
         self.patch_send = asynctest.patch.object(self.bot, 'send_message')
         self.mock_send = self.patch_send.start()
-        self._add_minecraft_channel()
+        self.games = []
+        self._add_game_channel()
 
     async def tearDown(self):
         self.patch_send.stop()
         self.patch_run.stop()
         self.patch_db.stop()
-        self.patch_mc.stop()
+        for game in self.games:
+            game['patch'].stop()
         await self.bot.close()
 
-    def _add_minecraft_channel(self):
-        self.mock_server_id = str(random.randrange(999999))
-        self.mock_channel_id = str(random.randrange(999999))
-        self.patch_mc = patch('bowser.bowser.Minecraft')
-        self.mock_mc = self.patch_mc.start()()
-        fake_data = {
+    def _add_game_channel(self):
+        game = {
+            'server_id': str(random.randrange(999999)),
+            'channel_id': str(random.randrange(999999)),
             'host': ''.join(random.choice(string.ascii_lowercase) for _ in range(10)),
             'port': random.randrange(65535),
         }
-        self.mock_mc.mc_server.host = fake_data['host']
-        self.mock_mc.mc_server.port = fake_data['port']
+        game['patch'] = patch('bowser.bowser.Minecraft')
+        game['mock'] = game['patch'].start()()
+        game['mock'].mc_server.host = game['host']
+        game['mock'].mc_server.port = game['port']
         self.bowser.db.set_data_of_server_channel(
-            self.mock_server_id,
-            self.mock_channel_id,
-            fake_data
+            game['server_id'],
+            game['channel_id'],
+            {'host': game['host'], 'port': game['port']},
         )
+        self.games.append(game)
 
     def _get_mock_command_message(self, command):
-        return self._get_mock_message(command, channel=self.mock_channel_id)
+        return self._get_mock_message(command, channel=self.games[0]['channel_id'])
 
     def _get_mock_channel(self, **kwargs):
         id = kwargs.pop('id', str(random.randrange(999999)))
@@ -61,7 +64,7 @@ class HelperFunctions(asynctest.TestCase):
     def _get_mock_server(self):
         return asynctest.MagicMock(
             spec=discord.Server,
-            id=self.mock_server_id,
+            id=self.games[0]['server_id'],
             me=self.bot.user,
         )
 
@@ -90,20 +93,20 @@ class HelperFunctions(asynctest.TestCase):
 
 class TestBowser(HelperFunctions):
     async def test__statuses_command_works(self):
-        mock_mcs = [self.mock_mc, self.mock_mc]
+        mock_mcs = [self.games[0]['mock'], self.games[0]['mock']]
         fake_data = {'host': 'fake_host', 'port': 123}
         for i, _ in enumerate(mock_mcs):
             mock_mcs[i].mc_server.host = fake_data['host'] + str(i)
             mock_mcs[i].mc_server.port = fake_data['port']
             self.bowser.db.set_data_of_server_channel(
-                self.mock_server_id,
-                self.mock_channel_id + str(i),
+                self.games[0]['server_id'],
+                self.games[0]['channel_id'] + str(i),
                 fake_data,
             )
         mock_message = self._get_mock_command_message('!statuses')
         await self.bot.on_message(mock_message)
         await asyncio.sleep(0.02)
-        mock_mcs.append(self.mock_mc)
+        mock_mcs.append(self.games[0]['mock'])
 
         self.mock_send.assert_called_once_with(
             mock_message.channel,
@@ -136,18 +139,18 @@ class TestBowser(HelperFunctions):
         )
 
     async def test__owner_can_add_a_server(self):
-        mock_message = self._get_mock_command_message(f'!set {self.mock_mc.mc_server.host} {self.mock_mc.mc_server.port}')
+        mock_message = self._get_mock_command_message(f"!set {self.games[0]['mock'].mc_server.host} {self.games[0]['mock'].mc_server.port}")
         mock_message.channel.permissions_for.return_value = discord.permissions.Permissions()
         mock_message.author = mock_message.server.owner
         await self.bot.on_message(mock_message)
         await asyncio.sleep(0.02)
         self.mock_send.assert_called_once_with(
             mock_message.channel,
-            f'Finished adding `{self.mock_mc.mc_server.host}:{self.mock_mc.mc_server.port}`.  Try `!status` now.',
+            f"Finished adding `{self.games[0]['mock'].mc_server.host}:{self.games[0]['mock'].mc_server.port}`.  Try `!status` now.",
         )
 
     async def test__nonadmin_cannot_add_a_server(self):
-        mock_message = self._get_mock_command_message(f'!set {self.mock_mc.mc_server.host} {self.mock_mc.mc_server.port}')
+        mock_message = self._get_mock_command_message(f"!set {self.games[0]['mock'].mc_server.host} {self.games[0]['mock'].mc_server.port}")
         mock_message.channel.permissions_for.return_value = discord.permissions.Permissions()
         await self.bot.on_message(mock_message)
         await asyncio.sleep(0.02)
@@ -159,34 +162,34 @@ class TestBowser(HelperFunctions):
     async def test__can_fetch_motd(self):
         mock_message = self._get_mock_command_message('!motd')
         await self.bot.on_message(mock_message)
-        self.mock_mc.get_motd.assert_called_once()
+        self.games[0]['mock'].get_motd.assert_called_once()
         self.mock_send.assert_called_once_with(
             mock_message.channel,
-            self.mock_mc.get_motd(),
+            self.games[0]['mock'].get_motd(),
         )
 
     async def test__can_fetch_forge_version(self):
         mock_message = self._get_mock_command_message('!forge_version')
         await self.bot.on_message(mock_message)
-        self.mock_mc.get_forge_version_message.assert_called_once()
+        self.games[0]['mock'].get_forge_version_message.assert_called_once()
         self.mock_send.assert_called_once_with(
             mock_message.channel,
-            self.mock_mc.get_forge_version_message(),
+            self.games[0]['mock'].get_forge_version_message(),
         )
 
     async def test__errors_in_command_execution_are_logged(self):
-        self.mock_mc.get_formatted_status_message.side_effect = Exception
+        self.games[0]['mock'].get_formatted_status_message.side_effect = Exception
         await self._assert_status_command_responds_with(
             'Ninjas hijacked the packets, but the author will fix it.')
 
     async def test__tells_the_user_when_the_ip_is_bad(self):
         from socket import gaierror
-        self.mock_mc.get_formatted_status_message.side_effect = gaierror
+        self.games[0]['mock'].get_formatted_status_message.side_effect = gaierror
         await self._assert_status_command_responds_with(
             'The !ip is unreachable; complain to someone in charge.')
 
     async def test__bot_gives_up_on_discord_command_errors(self):
-        self.mock_mc.get_formatted_status_message.side_effect = \
+        self.games[0]['mock'].get_formatted_status_message.side_effect = \
             discord.ext.commands.errors.CommandError
         await self._assert_status_command_responds_with(
             'The bot is giving up; something unknown happened.')
@@ -201,38 +204,38 @@ class TestBowser(HelperFunctions):
         await self.bot.on_message(mock_message)
         self.mock_send.assert_called_once_with(
             mock_message.channel,
-            f'{self.mock_mc.mc_server.host}:{self.mock_mc.mc_server.port}',
+            f"{self.games[0]['mock'].mc_server.host}:{self.games[0]['mock'].mc_server.port}",
         )
 
     async def test__status_command_warns_about_missing_server(self):
         mock_message = self._get_mock_command_message('!status')
         mock_message.channel = self._get_mock_channel(id='some unconfigured channel')
         await self.bot.on_message(mock_message)
-        self.mock_mc.get_formatted_status_message.assert_not_called()
+        self.games[0]['mock'].get_formatted_status_message.assert_not_called()
         await asyncio.sleep(0.02)
         self.mock_send.assert_called_once_with(mock_message.channel,
             'There is not yet a Minecraft server configured for this discord'
             ' server channel.')
 
     async def test__status_command_when_the_server_does_not_respond(self):
-        self.mock_mc.get_formatted_status_message.side_effect = OSError
+        self.games[0]['mock'].get_formatted_status_message.side_effect = OSError
         await self._assert_status_command_responds_with(
             'Server did not respond with any information.')
 
     async def test__status_command_responds_even_with_connection_errors(self):
-        self.mock_mc.get_formatted_status_message.side_effect = \
+        self.games[0]['mock'].get_formatted_status_message.side_effect = \
             ConnectionRefusedError
         await self._assert_status_command_responds_with(
             'The server is not accepting connections at this time.')
 
     async def test__status_command_responds_with_status_message(self):
-        msg = self.mock_mc.get_formatted_status_message()
-        self.mock_mc.get_formatted_status_message.reset_mock()
+        msg = self.games[0]['mock'].get_formatted_status_message()
+        self.games[0]['mock'].get_formatted_status_message.reset_mock()
         await self._assert_status_command_responds_with(msg)
 
     async def _assert_status_command_responds_with(self, message):
         mock_message = self._get_mock_command_message('!status')
         await self.bot.on_message(mock_message)
-        self.mock_mc.get_formatted_status_message.assert_called_once()
+        self.games[0]['mock'].get_formatted_status_message.assert_called_once()
         await asyncio.sleep(0.02)
         self.mock_send.assert_called_once_with(mock_message.channel, message)
