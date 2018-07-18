bc6fa8f0281d19b406b45b1bfcb9507ec3f2806b
Kevin Tindall <kevinkjt2000@gmail.com>
2018-07-17 22:19:06 -0500

    test: Use unique MagicMocks in tests

--- test/test_bowser.py
+++ test/test_bowser.py
@@ -18,7 +18,8 @@ class HelperFunctions(asynctest.TestCase):
         self.addCleanup(patch_db.stop)
 
         self.patch_mc = patch('bowser.bowser.Minecraft')
-        self.mock_mc = self.patch_mc.start()()
+        self.mock_mc = self.patch_mc.start()
+        self.mock_mc.side_effect = []
         self.addCleanup(self.patch_mc.stop)
 
         self.bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
@@ -52,7 +53,8 @@ class HelperFunctions(asynctest.TestCase):
             game['channel_id'],
             {'host': game['host'], 'port': game['port']},
         )
-        game['mock'] = self.mock_mc
+        game['mock'] = asynctest.MagicMock()
+        self.mock_mc.side_effect = list(self.mock_mc.side_effect) + [game['mock']]
         self.games.append(game)
 
     def _get_mock_command_message(self, command):
@@ -157,34 +159,34 @@ class TestBowser(HelperFunctions):
     async def test__can_fetch_motd(self):
         mock_message = self._get_mock_command_message('!motd')
         await self.bot.on_message(mock_message)
-        self.mock_mc.get_motd.assert_called_once()
+        self.games[0]['mock'].get_motd.assert_called_once()
         self.mock_send.assert_called_once_with(
             mock_message.channel,
-            self.mock_mc.get_motd(),
+            self.games[0]['mock'].get_motd(),
         )
 
     async def test__can_fetch_forge_version(self):
         mock_message = self._get_mock_command_message('!forge_version')
         await self.bot.on_message(mock_message)
-        self.mock_mc.get_forge_version_message.assert_called_once()
+        self.games[0]['mock'].get_forge_version_message.assert_called_once()
         self.mock_send.assert_called_once_with(
             mock_message.channel,
-            self.mock_mc.get_forge_version_message(),
+            self.games[0]['mock'].get_forge_version_message(),
         )
 
     async def test__errors_in_command_execution_are_logged(self):
-        self.mock_mc.get_formatted_status_message.side_effect = Exception
+        self.games[0]['mock'].get_formatted_status_message.side_effect = Exception
         await self._assert_status_command_responds_with(
             'Ninjas hijacked the packets, but the author will fix it.')
 
     async def test__tells_the_user_when_the_ip_is_bad(self):
         from socket import gaierror
-        self.mock_mc.get_formatted_status_message.side_effect = gaierror
+        self.games[0]['mock'].get_formatted_status_message.side_effect = gaierror
         await self._assert_status_command_responds_with(
             'The !ip is unreachable; complain to someone in charge.')
 
     async def test__bot_gives_up_on_discord_command_errors(self):
-        self.mock_mc.get_formatted_status_message.side_effect = \
+        self.games[0]['mock'].get_formatted_status_message.side_effect = \
             discord.ext.commands.errors.CommandError
         await self._assert_status_command_responds_with(
             'The bot is giving up; something unknown happened.')
@@ -199,38 +201,38 @@ class TestBowser(HelperFunctions):
         await self.bot.on_message(mock_message)
         self.mock_send.assert_called_once_with(
             mock_message.channel,
-            f"{self.mock_mc.mc_server.host}:{self.mock_mc.mc_server.port}",
+            f"{self.games[0]['mock'].mc_server.host}:{self.games[0]['mock'].mc_server.port}",
         )
 
     async def test__status_command_warns_about_missing_server(self):
         mock_message = self._get_mock_command_message('!status')
         mock_message.channel = self._get_mock_channel(id='some unconfigured channel')
         await self.bot.on_message(mock_message)
-        self.mock_mc.get_formatted_status_message.assert_not_called()
+        self.games[0]['mock'].get_formatted_status_message.assert_not_called()
         await asyncio.sleep(0.02)
         self.mock_send.assert_called_once_with(mock_message.channel,
             'There is not yet a Minecraft server configured for this discord'
             ' server channel.')
 
     async def test__status_command_when_the_server_does_not_respond(self):
-        self.mock_mc.get_formatted_status_message.side_effect = OSError
+        self.games[0]['mock'].get_formatted_status_message.side_effect = OSError
         await self._assert_status_command_responds_with(
             'Server did not respond with any information.')
 
     async def test__status_command_responds_even_with_connection_errors(self):
-        self.mock_mc.get_formatted_status_message.side_effect = \
+        self.games[0]['mock'].get_formatted_status_message.side_effect = \
             ConnectionRefusedError
         await self._assert_status_command_responds_with(
             'The server is not accepting connections at this time.')
 
     async def test__status_command_responds_with_status_message(self):
-        msg = self.mock_mc.get_formatted_status_message()
-        self.mock_mc.get_formatted_status_message.reset_mock()
+        msg = self.games[0]['mock'].get_formatted_status_message()
+        self.games[0]['mock'].get_formatted_status_message.reset_mock()
         await self._assert_status_command_responds_with(msg)
 
     async def _assert_status_command_responds_with(self, message):
         mock_message = self._get_mock_command_message('!status')
         await self.bot.on_message(mock_message)
-        self.mock_mc.get_formatted_status_message.assert_called_once()
+        self.games[0]['mock'].get_formatted_status_message.assert_called_once()
         await asyncio.sleep(0.02)
         self.mock_send.assert_called_once_with(mock_message.channel, message)
