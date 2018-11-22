30076120aa8042931e9e7fe08b4cb68c2025b467
Kevin Tindall <kevinkjt2000@gmail.com>
2018-11-21 23:09:55 -0600

    Remove token.txt from EC2 deployment

--- scripts/ec2-backup.sh
+++ scripts/ec2-backup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mkdir -p backup/redis-data
 docker-machine scp bowser:./redis-data/* backup/redis-data/
-docker-machine scp bowser:./token.txt backup/
--- scripts/ec2-update.sh
+++ scripts/ec2-update.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-make package
 
-docker-machine scp dist/requirements.pex bowser:.
 docker-machine scp dist/bowser.pex bowser:.
-docker-machine scp token.txt bowser:.
 
 eval $(docker-machine env bowser)
 docker-compose -f docker-compose.yml -f docker-compose.ec2.yml up -d
