642a36adb11033227895b3e9978d7438c7363938
Kevin Tindall <kevinkjt2000@gmail.com>
2018-11-21 23:07:11 -0600

    Update to newer ami id of coreos

--- scripts/ec2-create.sh
+++ scripts/ec2-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker-machine create \
   --driver=amazonec2 \
-  --amazonec2-ami=ami-3f061b45 \
+  --amazonec2-ami=ami-03ed1c12a1dd84320 \
   --amazonec2-device-name=/dev/xvda \
   --amazonec2-ssh-user=core \
   bowser
