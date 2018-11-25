03fb79a575a4523f7e1611d6b325bc2a48309413
Kevin Tindall <kevinkjt2000@gmail.com>
2018-11-25 00:16:37 -0600

    Updated production scripts

--- scripts/ec2-update.sh
+++ scripts/ec2-update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker-machine scp dist/bowser.pex bowser:.
-
 eval $(docker-machine env bowser)
+docker-compose -f docker-compose.yml -f docker-compose.ec2.yml build
 docker-compose -f docker-compose.yml -f docker-compose.ec2.yml up -d
-docker-compose -f docker-compose.yml -f docker-compose.ec2.yml restart
