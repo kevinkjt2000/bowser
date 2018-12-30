4eab73d27b6d54d58791906ee34a35af9a6cada0
Kevin Tindall <kevinkjt2000@gmail.com>
2018-12-29 18:19:34 -0600

    Update nostrum to latest from git

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Bowser.MixProject do
       {:distillery, "~> 2.0", runtime: false},
       {:jason, "~> 1.1"},
       {:mcping, git: "https://github.com/kevinkjt2000/mcping-elixir.git"},
-      {:nostrum, git: "https://github.com/kevinkjt2000/nostrum.git"},
+      {:nostrum, git: "https://github.com/Kraigie/nostrum.git"},
       {:redix, "~> 0.8"},
       {:remix, git: "https://github.com/harlantwood/remix.git", only: :dev}
     ]
