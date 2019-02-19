87918ee60d3365ec12525115a9d9553f7e0e54bf
Kevin Tindall <kevinkjt2000@gmail.com>
2019-02-18 19:33:26 -0600

    Remove all the EC2 related things

--- scripts/ec2-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker-machine create \
-  --driver=amazonec2 \
-  --amazonec2-ami=ami-03ed1c12a1dd84320 \
-  --amazonec2-device-name=/dev/xvda \
-  --amazonec2-ssh-user=core \
-  bowser
-
-scripts/ec2-update.sh
