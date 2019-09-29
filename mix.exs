772beb40f7e178821a70e10d200d6eabdfac926a
Kevin Tindall <kevinkjt2000@gmail.com>
2019-09-29 12:42:26 -0500

    Don't start the app when testing

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Bowser.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      aliases: [test: "test --no-start"],
       deps: deps()
     ]
   end
