b488e2c099340d321e63b4b7941dd449179209f2
Kevin Tindall <kevinkjt2000@gmail.com>
2019-09-29 12:04:16 -0500

    Alphabetize and document dev config

--- config/config.exs
+++ config/config.exs
@@ -1,21 +1,22 @@
 use Mix.Config
 
 if Mix.env() == :dev do
-  config :remix,
-    escript: false,
-    silent: true
+  # Even though production also uses REDIS_PORT and BOT_TOKEN,
+  # those variables must be given via runtime configuration instead.
+  # (see: rel/config/runtime.exs)
+  config :bowser,
+    redis_host: "localhost",
+    redis_port: System.get_env("REDIS_PORT")
 
   config :nostrum,
     token: System.get_env("BOT_TOKEN")
 
-  config :bowser,
-    redis_host: "localhost"
+  config :remix,
+    escript: false,
+    silent: true
 end
 
 if Mix.env() == :prod do
   config :bowser,
     redis_host: "redis"
 end
-
-config :bowser,
-  redis_port: System.get_env("REDIS_PORT")
