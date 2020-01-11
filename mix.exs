6cc5018804d68417f3cd9aff36d7edb2d62657c7
Kevin Tindall <kevinkjt2000@gmail.com>
2020-01-11 15:09:56 -0600

    Treat warnings as errors in prod/dev

--- mix.exs
+++ mix.exs
@@ -9,7 +9,10 @@ defmodule Bowser.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: [test: "test --no-start"],
       releases: releases(),
-      deps: deps()
+      deps: deps(),
+      elixirc_options: [
+        warnings_as_errors: halt_on_warnings?(Mix.env())
+      ]
     ]
   end
 
@@ -23,6 +26,9 @@ defmodule Bowser.MixProject do
 
   defp extra_applications(:dev), do: extra_applications(:all) ++ [:remix]
   defp extra_applications(_all), do: [:logger, :nostrum]
+
+  defp halt_on_warnings?(:test), do: false
+  defp halt_on_warnings?(_), do: true
 
   defp releases() do
     [
