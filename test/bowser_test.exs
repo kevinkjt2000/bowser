58f48d876a7143320ef3b27f286180d37b55de76
Kevin Tindall <kevinkjt2000@gmail.com>
2020-01-11 10:04:41 -0600

    Create contract boundaries for discord/redis

--- test/bowser_test.exs
+++ test/bowser_test.exs
@@ -1,8 +1,13 @@
 defmodule BowserTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Bowser
 
-  test "there are no problems here" do
-    assert true
+  describe "ip_command/1" do
+    test "sends a message containing the host and port configured for the channel" do
+      msg = %{guild_id: "7890", channel_id: 123_456}
+      Bowser.ip_command(msg)
+
+      assert "create_message was called with ?"
+    end
   end
 end
